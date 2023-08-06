988e0d9b7f3b0b30e0891be5e45ca3c8dc5aa620
KlimentinaIvanova <klimentina1988@gmail.com>
2023-08-06 15:02:39 +0300

    Adding Navbar

--- blogwebsite/src/App.js
+++ blogwebsite/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Homepage from './Components/Homepage';
+import Navbar from './Components/navbar';
 
 function App() {
   return (
     <div className="App">
-    
+    <Navbar/>
       <Homepage/>
     </div>
   );
