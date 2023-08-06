724f678e7dfc60a7a56e7c8658081935b5058d53
KlimentinaIvanova <klimentina1988@gmail.com>
2023-08-06 13:56:05 +0300

    Adding store.js

--- blogwebsite/src/app/store.js
+++ blogwebsite/src/app/store.js
@@ -1,1 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import  useReducer  from "../features/userSlice";
+
+export default configureStore({
+    reducer:{
+        user: useReducer,
+    },
+})
